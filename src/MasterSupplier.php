3628c78eb3c556f6ee95a26b1f21d6e05db58604
Kosuke Shibuya <shibuya@imagenious.co.jp>
2020-11-15 22:22:11 +0900

    add supplier api (#8)
    * add supplier api
    
    * mod readme

---
+++ src/MasterSupplier.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace ShibuyaKosuke\LaravelNextEngine;
+
+use ShibuyaKosuke\LaravelNextEngine\Entities\MasterSupplier\MasterSupplier as MasterSupplierBase;
+
+/**
+ * 仕入先マスタ
+ *
+ * Trait MasterSupplier
+ * @package ShibuyaKosuke\LaravelNextEngine
+ */
+trait MasterSupplier
+{
+    /**
+     * 仕入先マスタ検索
+     *
+     * @param array $params
+     * @return ApiResultEntity
+     */
+    public function receiveMasterSupplierSearch(array $params = []): ApiResultEntity
+    {
+        $params = array_merge(
+            [
+                'fields' => MasterSupplierBase::getPropertiesString(),
+                'access_token' => $this->access_token,
+                'refresh_token' => $this->refresh_token,
+                'wait_flag' => $this->getWaitFlag(),
+            ],
+            $params
+        );
+
+        $response = $this->apiExecute(MasterSupplierBase::$endpoint_search, $params);
+        return new ApiResultEntity(MasterSupplierBase::setData($response));
+    }
+
+    /**
+     * 仕入先マスタ件数
+     *
+     * @param array $params
+     * @return ApiResultEntity
+     */
+    public function receiveMasterSupplierCount(array $params = []): ApiResultEntity
+    {
+        $params = array_merge(
+            [
+                'fields' => MasterSupplierBase::getPropertiesString(),
+                'access_token' => $this->access_token,
+                'refresh_token' => $this->refresh_token,
+                'wait_flag' => $this->getWaitFlag(),
+            ],
+            $params
+        );
+
+        $response = $this->apiExecute(MasterSupplierBase::$endpoint_count, $params);
+        return new ApiResultEntity(MasterSupplierBase::setData($response));
+    }
+}
