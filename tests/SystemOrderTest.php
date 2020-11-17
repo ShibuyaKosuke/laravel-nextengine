5091657bf59530ea087e55f488bfb60f5e3fe738
Kosuke Shibuya <shibuya@imagenious.co.jp>
2020-11-17 12:49:49 +0900

    Feature/system order (#27)
    * add system order
    
    * add system order condition

---
+++ tests/SystemOrderTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace ShibuyaKosuke\LaravelNextEngine\Tests;
+
+use ShibuyaKosuke\LaravelNextEngine\Entities\SystemOrder\SystemOrder;
+use ShibuyaKosuke\LaravelNextEngine\Entities\SystemOrder\SystemOrderCondition;
+use ShibuyaKosuke\LaravelNextEngine\Facades\NextEngine;
+use ShibuyaKosuke\LaravelNextEngine\Models\NextEngineApi;
+
+/**
+ * 発注区分情報
+ *
+ * Class SystemOrderTest
+ * @package ShibuyaKosuke\LaravelNextEngine\Tests
+ */
+class SystemOrderTest extends TestCase
+{
+    private $object;
+
+    /**
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->artisan('migrate:fresh');
+
+        $this->getEnvironmentSetUp($this->app);
+
+        $this->nextEngineApi = factory(NextEngineApi::class)->make();
+
+        $this->nextEngineApi->uid = env('NEXT_ENGINE_UID');
+        $this->nextEngineApi->state = env('NEXT_ENGINE_STATE');
+
+        $response = NextEngine::setAccount($this->nextEngineApi)->getAccessToken();
+
+        $this->object = NextEngine::setAccount($this->nextEngineApi);
+    }
+
+    /**
+     * 発注区分情報
+     */
+    public function testSystemOrder()
+    {
+        $apiResultEntity = $this->object->systemOrder();
+
+        $this->assertEqualsApiResponseSearch($apiResultEntity);
+
+        foreach ($apiResultEntity->data as $data) {
+            self::assertInstanceOf(SystemOrder::class, $data);
+        }
+    }
+
+    /**
+     * 発注条件区分情報
+     */
+    public function testSystemOrderCondition()
+    {
+        $apiResultEntity = $this->object->systemOrderCondition();
+
+        $this->assertEqualsApiResponseSearch($apiResultEntity);
+
+        foreach ($apiResultEntity->data as $data) {
+            self::assertInstanceOf(SystemOrderCondition::class, $data);
+        }
+    }
+}
